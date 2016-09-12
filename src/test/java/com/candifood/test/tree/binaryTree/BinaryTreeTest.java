739567b60babc1a8cc5127c904e4478ca2cd0493
aniruddha das <aniruddhadas9@gmail.com>
2016-09-12 15:23:16 -0400

    changed the project structure

---
+++ src/test/java/com/candifood/test/tree/binaryTree/BinaryTreeTest.java
@@ -0,0 +1,15 @@
+package com.candifood.test.tree.binaryTree;
+
+import org.junit.Test;
+
+/**
+ * Created by adas008c on 9/12/2016.
+ */
+public class BinaryTreeTest {
+
+    @Test
+    public void BinaryTreeOperation() {
+
+
+    }
+}
