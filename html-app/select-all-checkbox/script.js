08a8025909e2b368faa8e948eb54701e115849f2
Aniruddha Das <aniruddhadas9@gmail.com>
2022-05-13 23:15:56 -0700

    - move angular to angular-app  - Add box selection program  - Initiate all checkbox selection process

--- html-app/select-all-checkbox/script.js
+++ html-app/select-all-checkbox/script.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log('onloadEvent');
     const container = document.getElementsByClassName('box');
+    const resetButton = document.getElementsByClassName('reset-button');
+    const valDiv = document.getElementsByClassName('val');
     console.log('container: %o', container);
 
     // foreach loop did not worked, need to debug later
     // container.forEach ((box) => {
     for (let i = 0; i < container.length; i++) {
         console.log('box: %o', container[i]);
-        container[i].innerHTML = 'c='+i;
-
+        // container[i].innerHTML = i;
         container[i].addEventListener("mousedown", function (event) {
             console.log('mouse down');
             event.target.style.backgroundColor = 'red';
@@ -16,12 +17,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
         container[i].addEventListener("mouseenter", function (event) {
             console.log('mouse enter');
+            // event.
 
             if (detectLeftButton(event)) {
-                if(i<=4) {
-                    for (let j = i; j < container.length; j++) {
-                        container[j].style.backgroundColor = 'red';
+                if(i <= 4) {
+                    let idt = i;
+                    let val = [i];
+                    for (let j = 0; j < 7; j++) {
+                        // container[idt].innerHTML = parseInt(container[idt].innerHTML, 10) + 'w';
+                        container[idt].style.backgroundColor = 'red';
+                        idt += 5;
+                        val.push(idt);
                     }
+                    valDiv[0].innerHTML = JSON.stringify(val);
                 }
                 event.target.style.backgroundColor = 'red';
             }
@@ -35,8 +43,26 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    resetButton[0].addEventListener('click', (event) => {
+        for (let i = 0; i < container.length; i++) {
+            // container[i].innerHTML = parseInt(container[i].innerHTML, 10) + i;
+            container[i].style.backgroundColor = 'white';
+        }
+    });
+
+
+
 
 });
+
+function resetBoxes() {
+    const container = document.getElementsByClassName('box');
+    for (let i = 0; i < container.length; i++) {
+        container[i].style.backgroundColor = 'none';
+    }
+}
+
+
 
 function detectLeftButton(event) {
     if (event.metaKey || event.ctrlKey || event.altKey || event.shiftKey) {
---
+++ html-app/square-diagonal-selection/script.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('onloadEvent');
+    const container = document.getElementsByClassName('box');
+    const resetButton = document.getElementsByClassName('reset-button');
+    const valDiv = document.getElementsByClassName('val');
+    console.log('container: %o', container);
+
+    // foreach loop did not worked, need to debug later
+    // container.forEach ((box) => {
+    for (let i = 0; i < container.length; i++) {
+        console.log('box: %o', container[i]);
+        // container[i].innerHTML = i;
+        container[i].addEventListener("mousedown", function (event) {
+            console.log('mouse down');
+            event.target.style.backgroundColor = 'red';
+        });
+
+        container[i].addEventListener("mouseenter", function (event) {
+            console.log('mouse enter');
+            // event.
+
+            if (detectLeftButton(event)) {
+                if(i <= 4) {
+                    let idt = i;
+                    let val = [i];
+                    for (let j = 0; j < 7; j++) {
+                        // container[idt].innerHTML = parseInt(container[idt].innerHTML, 10) + 'w';
+                        container[idt].style.backgroundColor = 'red';
+                        idt += 5;
+                        val.push(idt);
+                    }
+                    valDiv[0].innerHTML = JSON.stringify(val);
+                }
+                event.target.style.backgroundColor = 'red';
+            }
+        });
+
+        container[i].addEventListener("mouseleave", function (event) {
+            console.log(event);
+            if (detectLeftButton(event)) {
+                // event.target.style.backgroundColor = 'white';
+            }
+        });
+    }
+
+    resetButton[0].addEventListener('click', (event) => {
+        for (let i = 0; i < container.length; i++) {
+            // container[i].innerHTML = parseInt(container[i].innerHTML, 10) + i;
+            container[i].style.backgroundColor = 'white';
+        }
+    });
+
+
+
+
+});
+
+function resetBoxes() {
+    const container = document.getElementsByClassName('box');
+    for (let i = 0; i < container.length; i++) {
+        container[i].style.backgroundColor = 'none';
+    }
+}
+
+
+
+function detectLeftButton(event) {
+    if (event.metaKey || event.ctrlKey || event.altKey || event.shiftKey) {
+        return false;
+    } else if ('buttons' in event) {
+        return event.buttons === 1;
+    } else if ('which' in event) {
+        return event.which === 1;
+    } else {
+        return (event.button == 1 || event.type == 'click');
+    }
+}
+
